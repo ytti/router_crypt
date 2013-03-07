d3d26c146c6b6bf6022bba22f2b1ca50ba0a329f
Saku Ytti <saku.ytti@tdc.fi>
2013-03-07 14:08:32 +0200

    Fix CLI when no argument is given
    Also supposedly just cosmetic change to rtrcrypt shebang, it's not
    really used anyhow.

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,7 +1,8 @@
 class RouterCrypt::CLI
   class << self
     def run
-      pw = ARGV[0].dup or raise ArgumentError, 'no password given'
+      ARGV[0] or raise ArgumentError, 'no password given'
+      pw = ARGV[0].dup 
       case pw
       when /^\$9\$/
         RouterCrypt::JunOS.decrypt pw
