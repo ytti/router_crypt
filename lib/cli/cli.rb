81c3b8c46cbbafeb7991f3c9c1368e6af38e9fc6
Jarkko Saloranta <jarkko.saloranta@dna.fi>
2016-11-22 01:27:04 +0200

    add junos crypt support

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,17 +1,43 @@
+require 'slop'
+require 'pp'
+
+
 class RouterCrypt::CLI
   class << self
     def run
-      ARGV[0] or raise ArgumentError, 'no password given'
-      pw = ARGV[0].dup 
-      case pw
-      when /^\$9\$/
-        RouterCrypt::JunOS.decrypt pw
-      when /^[\dA-F]+$/
-        RouterCrypt::IOS.decrypt pw
-      else
-        #presume it's NXOS, no clear way to separate garbage and NXOS PW
-        RouterCrypt::NXOS.decrypt pw
+      opts=opts_parse.to_hash
+      #ARGV[0] or raise ArgumentError, 'no password given'
+
+      if opts[:junipercrypt]
+        pw=opts[:junipercrypt]
+	if opts[:salt]
+          RouterCrypt::JunOS.crypt(opts[:junipercrypt], opts[:salt])
+        else
+          RouterCrypt::JunOS.crypt opts[:junipercrypt]
+        end
+      elsif opts [:decrypt]
+        pw=opts[:decrypt].dup
+        case pw
+        when /^\$9\$/
+          RouterCrypt::JunOS.decrypt pw
+        when /^[\dA-F]+$/
+          RouterCrypt::IOS.decrypt pw
+        else
+          #presume it's NXOS, no clear way to separate garbage and NXOS PW
+          RouterCrypt::NXOS.decrypt pw
+        end
       end
     end
+
+    def opts_parse
+      Slop.parse do |o|
+        o.bool '-D', '--debug', 'turn on debugging'
+        o.on   '-h', '--help' do puts o; exit; end
+        o.string  '-j','--junipercrypt', 'crypt Juniper'
+        o.string  '-s','--salt', 'salt for crypt'
+	o.string  '-d','--decrypt', 'decrypt'
+      end
+    end
+
   end
 end
--- lib/junos/common.rb
+++ lib/junos/common.rb
@@ -1,40 +1,77 @@
 class RouterCrypt::JunOS
   class InvalidPW < StandardError; end
-  
+
   ENCODE = [
-      [ 1,  4, 32 ],
-      [ 1, 16, 32 ],
-      [ 1,  8, 32 ],
-      [ 1, 64     ],
-      [ 1, 32     ],
-      [ 1, 4, 16, 128 ],
-      [ 1, 32, 64 ],
+    [ 1,  4, 32 ],
+    [ 1, 16, 32 ],
+    [ 1,  8, 32 ],
+    [ 1, 64     ],
+    [ 1, 32     ],
+    [ 1, 4, 16, 128 ],
+    [ 1, 32, 64 ],
   ]
   EXTRA   = {}
   KEY     = %w( QzF3n6/9CAtpu0O B1IREhcSyrleKvMW8LXx 7N-dVbwsY2g4oaJZGUDj iHkq.mPf5T )
   KEYCHAR = KEY.join.each_char.to_a
+  CHARKEY = {}
+  for q in 0..(KEYCHAR.length - 1)
+    CHARKEY[KEYCHAR[q]] = q;
+  end
   KEY.each_with_index do |key, index|
     key.each_char { |c| EXTRA[c] = 3-index }
   end
-  
+
   class << self
-  
-    private 
- 
+
+    private
+
     def nibble str, len
       nib, str[0..len-1] = str[0..len-1], ''
       nib.size == len or raise InvalidPW, 'Insufficent amont of characters'
       nib
     end
-  
+
     def gap c1, c2
-      (KEYCHAR.index(c1) - KEYCHAR.index(c2)) % KEYCHAR.size - 1 
+      (KEYCHAR.index(c1) - KEYCHAR.index(c2)) % KEYCHAR.size - 1
     end
-  
+
     def gap_decode gaps, dec
       gaps.size == dec.size or raise InvalidPW, 'gaps and dec are unequal size'
       (gaps.each_with_index.inject(0) { |num, (e, index)| num + e * dec[index] } % 256).chr
     end
-  
+
+    ## return a random number of characters from our alphabet
+    def randc cnt
+      r = ''
+      while cnt>0
+        r << KEYCHAR[(rand KEYCHAR.length).to_i]
+        cnt-=1
+      end
+      r
+    end
+
+    ## encode a plain-text character with a series of gaps,
+    ## according to the current encoder.
+    def gap_encode  pc,prev,enc
+      ord = pc.ord
+
+      crypt = ''
+      gaps=[]
+
+      enc.reverse.each do |x|
+        gaps.unshift (ord/x).to_i
+        ord %= x
+      end
+
+      gaps.each do |gap|
+        gap += CHARKEY[prev]+1
+        c = KEYCHAR[gap % KEYCHAR.length]
+        prev = c
+        crypt<<c
+      end
+
+      crypt
+    end
+
   end
 end
